e34735ea3113966cc098e4a8d87ab91ff4c73062
Sang Hyuk Lee <seobqod@gmail.com>
2022-02-16 19:19:14 +0900

    Update 최댓값과_최솟값.py

--- programmers/level2/최댓값과_최솟값.py
+++ programmers/level2/최댓값과_최솟값.py
@@ -34,3 +34,11 @@ def solution(s):
 def solution(s):
     s = list(map(int,s.split()))
     return str(min(s)) + " " + str(max(s))
+
+
+
+# D+1
+
+def solution(s):
+    s=list(map(int,s.split(' ')))
+    return f"{min(s)} {max(s)}"
