e69048a4ab8e0ca856c15fc78f4b97e38000efad
Sang Hyuk Lee <seobqod@gmail.com>
2022-02-18 01:51:04 +0900

    Update 땅따먹기.py

--- programmers/level2/땅따먹기.py
+++ programmers/level2/땅따먹기.py
@@ -5,6 +5,7 @@
 # 10 11 12 11
 # 4(12) 3(12) 2(11) 1(12)
 # 그때그때 바로 이전의 같은 열의 원소 제외하고 최댓값을 선택해서 더해 내려가면 됨.
+# 몇 개 해보니까 규칙이 있다고 생각했고, land 그대로 수정하면 된다고 생각했음.
 
 def solution(land):
     answer = 0
@@ -19,3 +20,17 @@ def solution(land):
 # dynamic programming ?
 # 메모이제이션 ?
 # 기억 안 남... 공부하기 - legacy
+
+# 다른 사람은 어떻게 풀었을까 ?
+
+# 짠게 막 다르지는 
+
+def solution(land):
+
+    for i in range(1, len(land)):
+        for j in range(len(land[0])):
+            land[i][j] = max(land[i -1][: j] + land[i - 1][j + 1:]) + land[i][j]
+
+    return max(land[-1])
+
+
